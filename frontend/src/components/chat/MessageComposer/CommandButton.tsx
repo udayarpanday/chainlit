d46057c4f492e43617fc31003af7f6226081a2d3
Udaya Panday <udaya.panday@gmail.com>
2025-03-27 14:04:03 +0545

    design updates

--- frontend/src/components/chat/MessageComposer/CommandButton.tsx
+++ frontend/src/components/chat/MessageComposer/CommandButton.tsx
@@ -25,6 +25,7 @@ import {
   TooltipProvider,
   TooltipTrigger
 } from '@/components/ui/tooltip';
+import { ChevronDown } from "lucide-react"
 
 interface Props {
   disabled?: boolean;
@@ -67,37 +68,39 @@ export const CommandButton = ({
       <PopoverContent
         align="start"
         sideOffset={12}
-        className="focus:outline-none"
+        className="focus:outline-none md:w-[700px] "
+
       >
         <Command className="rounded-lg border shadow-none">
-          <div className="flex items-center px-3 pt-3 pb-2">
+          <div className="flex items-center px-3 pt-3 pb-0">
             <CommandInput placeholder="Search prompts..." className="h-9" />
           </div>
-          <CommandList className="max-h-[400px] overflow-auto flex">
+          <CommandList className="max-h-[500px] overflow-auto flex">
             <CommandEmpty>No results found.</CommandEmpty>
             <div className="flex">
-              <CommandGroup>
+              <CommandGroup className='w-[200px] p-2'>
                 {commands.map((command) => (
                   <CommandItem
                     key={command.id}
                     onSelect={() => onCommandSelect(command)}
-                    className="command-item cursor-pointer px-3 py-3 rounded-none"
+                    className="command-item cursor-pointer px-3 py-3 justify-between rounded-md"
                   >
-                    <Icon
-                      name={command.icon}
-                      className="!size-5 text-muted-foreground"
-                    />
                     <div>
                       <div className="font-medium">{command.id}</div>
-                      <div className="text-sm text-muted-foreground">
-                        {command?.description ?? commands[0].description}
-                      </div>
+                    </div>
+                    <div className="text-gray-400">
+                      <ChevronDown className="h-5 w-5 rotate-[-90deg]" />
                     </div>
                   </CommandItem>
                 ))}
               </CommandGroup>
-              <div>
-                <p>{selectedCommand?.description ?? commands[0].description}</p>
+              <div className='border-l-2 md:w-[400px] hidden md:block'>
+                  <div class="flex-1 overflow-y-auto p-6">
+                    <div class="bg-gray-50 rounded-md p-4 relative">
+                      <p class="text-sm text-gray-700 whitespace-pre-wrap font-sans">{selectedCommand?.description ?? commands[0].description}</p>
+                      </div>
+                    </div>
+                <p></p>
               </div>
             </div>
           </CommandList>
